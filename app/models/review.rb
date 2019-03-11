0be251db86755fc674688229da55195d1d6a8a27
thomasshutton <thutton1983@gmail.com>
2019-03-10 22:40:06 -0400

    add data validation; recheck on address validation for new vets

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,13 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :Veterinarian, optional: true
 
+  validates :rating, :comment, presence: true
+
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1,
+    less_than_or_equal_to: 5,
+    message: "must be a whole number between 1 and 5"
+  }
+
 end
